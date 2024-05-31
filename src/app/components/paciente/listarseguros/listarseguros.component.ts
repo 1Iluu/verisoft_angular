4c61ff54c80c3c62e335e5ff94df15fcb1eec9b7
1Iluu <iguana29x@gmail.com>
2024-05-31 11:12:53 -0500

    Etructura

---
+++ src/app/components/paciente/listarseguros/listarseguros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarseguros',
+  standalone: true,
+  imports: [],
+  templateUrl: './listarseguros.component.html',
+  styleUrl: './listarseguros.component.css'
+})
+export class ListarsegurosComponent {
+
+}
