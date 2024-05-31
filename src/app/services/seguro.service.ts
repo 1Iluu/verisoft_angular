90e18e7789b938fd8cd5ae47b8e195b9d81dd8ba
1Iluu <iguana29x@gmail.com>
2024-05-31 13:28:03 -0500

    Servicio seguro

--- src/app/services/seguro.service.ts
+++ src/app/services/seguro.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Seguro } from '../models/seguro';
 
+
+
+const base_url=environment.csbase
 @Injectable({
   providedIn: 'root'
 })
 export class SeguroService {
+  private url = '$(base_url)/seguros'
 
-  constructor() { }
+  constructor(private hhtp:HttpClient) { }
+  list(){
+    return this.hhtp.get<Seguro[]>(this.url)
+  }
 }
