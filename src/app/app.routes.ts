2cfaca4458bd62ecf2a2fe3774638cbfe28b960b
1Iluu <iguana29x@gmail.com>
2024-05-31 22:33:27 -0500

    landin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { PacienteComponent } from './components/paciente/paciente.component';
+import { LandinpageComponent } from './components/landinpage/landinpage.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {
-        path:'seguro',component:PacienteComponent,
-        
-
+        path: '',
+        redirectTo: 'landing', pathMatch: 'full'
+      },
+    {
+        path:'landin',component:LandinpageComponent
+    },
+    {
+        path:'login',component:LoginComponent
+    },
+    {
+        path:'seguro',component:PacienteComponent 
     }
 ];
