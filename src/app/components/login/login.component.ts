6369d04a92d0aeb41ab3ab290cc53b7784d912a3
1Iluu <iguana29x@gmail.com>
2024-05-31 23:53:04 -0500

    arreglando el login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,MatFormFieldModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  username: string = ""
+  password: string = ""
+  mensaje: string = ""
+  login() {}
+  cambiarFormulario(){}
 }
