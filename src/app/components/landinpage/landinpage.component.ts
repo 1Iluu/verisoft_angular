79fff59a75aa6953b86911579c87d8a9c22a675f
1Iluu <iguana29x@gmail.com>
2024-06-02 16:44:07 -0500

    Mejorando el landing page

--- src/app/components/landinpage/landinpage.component.ts
+++ src/app/components/landinpage/landinpage.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-landinpage',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './landinpage.component.html',
   styleUrl: './landinpage.component.css'
 })
