e87abf41e3de17f928f19e1a446bd85d1e094c1b
1Iluu <iguana29x@gmail.com>
2024-06-05 07:40:15 -0500

    add new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,21 +3,38 @@ import { Routes } from '@angular/router';
 import { LandinpageComponent } from './components/landinpage/landinpage.component';
 import { LoginComponent } from './components/login/login.component';
 import { ListarsegurosComponent } from './components/paciente/listarseguros/listarseguros.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
+import { CrearpacienteComponent } from './components/paciente/crearpaciente/crearpaciente.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'landing', pathMatch: 'full'
+    
+  {
+    path: '',
+    redirectTo: 'landing', pathMatch: 'full'
+  },
+{
+    path:'landin',
+    component:LandinpageComponent
+},
+{
+    path:'',component:LandinpageComponent
+},
+{
+    path:'seguros',component:PacienteComponent ,
+    children:[
+      {
+          path:'nuevo',component:CrearpacienteComponent
       },
       {
-        path: 'landing', component: LandinpageComponent
+          path:'ediciones/:id',component:CrearpacienteComponent
       },
-      {
-        path: 'login', component: LoginComponent
-      },
-      {
-        path: 'seguros', component: ListarsegurosComponent
-      },
+
+  ]
+
+},
+{
+    path:'login',component:LoginComponent
+}
 
     
 ];
---
+++ src/app/components/paciente/crearpaciente/crearpaciente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crearpaciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './crearpaciente.component.html',
+  styleUrl: './crearpaciente.component.css'
+})
+export class CrearpacienteComponent {
+
+}
