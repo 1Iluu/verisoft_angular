36d860afd2f9428d317bfab4d960fe0c702051e2
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-06-10 23:24:31 -0500

    Update listaralergias.component.ts
    8

--- src/app/components/oncologo/listaralergias/listaralergias.component.ts
+++ src/app/components/oncologo/listaralergias/listaralergias.component.ts
@@ -1,31 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { Alergias } from '../../../models/alergias';
 import { AlergiasService } from '../../../services/alergias.service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
-export interface PeriodicElement {
-  idalergias: number;
-  Descripcionalergias: string;
-}
+
 @Component({
   selector: 'app-listaralergias',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule,
+    MatButtonModule, 
+    RouterLink,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
   templateUrl: './listaralergias.component.html',
   styleUrl: './listaralergias.component.css'
 })
 export class ListaralergiasComponent implements OnInit{
+  dataSource: MatTableDataSource<Alergias> = new MatTableDataSource();
+
   displayedColumns: string[]=
   [
     'id',
-    'descripcion'
+    'descripcion',
+    'accion01',
+    'accion02'
   ];
-  
-  dataSource:MatTableDataSource<Alergias> = new MatTableDataSource()
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private aS:AlergiasService ){}
   ngOnInit(): void {
     this.aS.list().subscribe((data)=>{
-      this.dataSource=new MatTableDataSource(data)
-    })
+      this.dataSource=new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+
+    });
+    this.aS.getList().subscribe((data)=>{
+      this.dataSource=new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+
+    });
 }
+  eliminar(id:number) {
+    this.aS.eliminar(id).subscribe((data)=>{
+      this.aS.list().subscribe((data)=> {
+        this.aS.setList(data);
+      });
+    });
+  }
 }
