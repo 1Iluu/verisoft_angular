ebda9b41b18b42527809ac992886b8452c764f92
Victor Alejandro Arévalo Díaz <u202112543@upc.edu.pe>
2024-06-11 06:58:15 -0500

    Creacion de carpetas(Parte 1) Users

---
+++ src/app/components/users/users-creaedita/users-creaedita.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-creaedita',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-creaedita.component.html',
+  styleUrl: './users-creaedita.component.css'
+})
+export class UsersCreaeditaComponent {
+
+}
