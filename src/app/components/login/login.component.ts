a025e0ae2360615c1352ab974e78547f06102fdd
1Iluu <iguana29x@gmail.com>
2024-06-11 08:43:20 -0500

    seguridad

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { JwtRequest } from '../../models/jwtRequest';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,10 +16,31 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  username: string = ""
-  password: string = ""
-  mensaje: string = ""
+export class LoginComponent implements OnInit{
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private snackbar: MatSnackBar,
+  ){}
   
-  cambiarFormulario(){}
+  username: string = "";
+  password: string = "";
+  mensaje: string = "";
+  ngOnInit(): void {}
+  login() {
+    let request = new JwtRequest();
+    request.username = this.username;
+    request.password = this.password;
+    this.loginService.login(request).subscribe(
+      (data: any) => {
+        sessionStorage.setItem('token', data.jwttoken);
+        this.router.navigate(['homes']);
+      },
+      (error) => {
+        this.mensaje = 'Credenciales incorrectas!!!';
+        this.snackbar.open(this.mensaje, 'Aviso', { duration: 2000 });
+      }
+    );
+  }
+  
 }
