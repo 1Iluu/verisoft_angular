4073caf08aec13d0a20f772fff5194aeef17725e
Nicoll23 <108904889+Nicoll23@users.noreply.github.com>
2024-06-10 23:24:56 -0500

    Update alergias.service.ts

--- src/app/services/alergias.service.ts
+++ src/app/services/alergias.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/enviroment';
 import { HttpClient } from '@angular/common/http';
 import { Alergias } from '../models/alergias';
+import { Subject } from 'rxjs';
 
 const base_url=environment.csbase
 @Injectable({
@@ -9,9 +10,28 @@ const base_url=environment.csbase
 })
 export class AlergiasService {
   private url=`${base_url}/alergias`
-  constructor(private http:HttpClient) { }
-
+  private listaCambio = new Subject<Alergias[]>();
+  constructor(private httpClient: HttpClient) {}
   list(){
-    return this.http.get<Alergias[]>(this.url);
+    return this.httpClient.get<Alergias[]>(this.url);
   }
+  insert(a: Alergias) {
+    return this.httpClient.post(this.url, a);
+  }
+  setList(listaNueva: Alergias[]) {
+    this.listaCambio.next(listaNueva);
+  }
+  getList() {
+    return this.listaCambio.asObservable();
+  }
+  listId(id: number) {
+    return this.httpClient.get<Alergias>(`${this.url}/${id}`);
+  }
+  update(a: Alergias) {
+    return this.httpClient.put(this.url, a);
+  }
+  eliminar(id: number) {
+    return this.httpClient.delete(`${this.url}/${id}`);
+  }
+  
 }
