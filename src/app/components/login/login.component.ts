267678cd9130d1eac6423a4687fc3a4bca47ad04
1Iluu <iguana29x@gmail.com>
2024-06-11 13:20:24 -0500

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { JwtRequest } from '../../models/jwtRequest';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,MatFormFieldModule,CommonModule, MatInputModule,MatButtonModule,RouterLink ],
+  imports: [FormsModule,MatFormFieldModule, MatInputModule,MatButtonModule, ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -36,7 +34,7 @@ export class LoginComponent implements OnInit{
     this.loginService.login(request).subscribe(
       (data: any) => {
         sessionStorage.setItem('token', data.jwttoken);
-        this.router.navigate(['homes']);
+        this.router.navigate(['home']);
       },
       (error) => {
         this.mensaje = 'Credenciales incorrectas!!!';
